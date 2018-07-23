98b93f6b7dfa867ddfea5e99f8b509e5598f963b
GMX9 <41052152+GMX9@users.noreply.github.com>
2018-07-23 16:53:11 +0100

    Add files via upload

---
+++ add_article.php
@@ -0,0 +1,213 @@
+<?php
+define('BASE_URL', 'http://spgeotecnia.w30.mycloud.pt');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$config['database']['host'] = "localhost";
+$config['database']['user'] = "spg";
+$config['database']['pass'] = "spg02";
+$config['database']['db'] = "spg";
+
+/*==========================  CUSTOM-PHP EDIT XML  ============================== */
+/*==========================     Version 3.0     ============================= */
+
+/*
+        Developed by Gonçalo M
+ */
+
+$connect = new mysqli($config['database']['host'],$config['database']['user'],$config['database']['pass'],$config['database']['db']);  
+
+
+// Verificar inputs que tenham 0 em XML_ADD
+
+$check = $connect->query("SELECT * FROM issueslist WHERE xml_add = 0");
+while($fetch = $check->fetch_array(MYSQLI_ASSOC)){
+
+
+$titulo = $fetch['titulo_pt'];
+$titulo_en = $fetch['titulo_en'];
+$ficheiro_link = $fetch['link'];
+$doi_link = $fetch['doi'];
+$first_page = $fetch['first_page'];
+$last_page = $fetch['last_page'];
+
+$xml_file = $fetch['link'];
+
+
+$url = BASE_URL."/uploads/";
+$file_url = $xml_file; // vem da db
+$original = $url;
+$replace = str_replace(array($url,".xml"),array("",""),$xml_file);
+$doi = $replace;
+
+
+
+
+//trim off excess whitespace off the whole
+$text = trim($fetch['autorxml']);
+//explode all separate lines into an array
+$textAr = explode("\n", $text);
+//trim all lines contained in the array.
+$textAr = array_filter($textAr, 'trim');
+//loop through the lines
+//var_dump($textAr);
+
+$linhas = preg_split('/\n|\r/',$text);
+$total_lines = count($linhas); 
+
+$xml_doc = new DomDocument;
+$xml_doc->formatOutput = true;
+$xml_doc->preserveWhiteSpace = false;
+$xml_doc->Load($xml_file);
+
+$journal = $xml_doc->getElementsByTagName('journal')->item(0);
+$journal_article = $xml_doc->createElement('journal_article');
+$journal_atr = $xml_doc->createAttribute('publication_type');
+// Value for the created attribute
+$journal_atr->value = 'full_text';
+
+
+$titles = $xml_doc->createElement('titles');
+$title = $xml_doc->createElement('title', $titulo_en);
+$original_language_title = $xml_doc->createElement('original_language_title',$titulo);
+
+
+$contributors = $xml_doc->createElement('contributors');
+
+$i = 0;
+foreach($textAr as $line) {
+ 
+
+    // Loop para diferenciar nomes para add childs
+    $arr = array('a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','x','y','z');  
+    $contrib = explode(",", $line);
+
+    //echo $contrib[0]."<br>";
+    //echo $contrib[1]."<br>";
+    //echo $contrib[2]."<br>";
+    //echo $contrib[3]."<br>";
+
+    $var = $arr[$i];
+   
+    ${"person_name_$i"} = $xml_doc->createElement('person_name');
+    ${"given_name_$i"} = $xml_doc->createElement('given_name',$contrib[0]);
+    ${"sur_name_$i"} = $xml_doc->createElement('sur_name',$contrib[1]);
+    ${"orcid_$i"} = $xml_doc->createElement('ORCID',$contrib[2]);
+    ${"organization_$i"} = $xml_doc->createElement('organization',$contrib[3]);
+    $i++; 
+
+}
+
+
+$mes = date('m');
+$ano = date("Y");
+
+$publication_date = $xml_doc->createElement('publication_date');
+$month = $xml_doc->createElement('month',$mes);
+$year = $xml_doc->createElement('year',$ano);
+$publication_date_o = $xml_doc->createElement('publication_date');
+$month_b = $xml_doc->createElement('month',$mes);
+$year_b = $xml_doc->createElement('year',$ano);
+$pub_Attribute = $xml_doc->createAttribute('media_type');
+// Value for the created attribute
+$pub_Attribute->value = 'print';
+$pub_Attribute_b = $xml_doc->createAttribute('media_type');
+// Value for the created attribute
+$pub_Attribute_b->value = 'online';
+$pages = $xml_doc->createElement('pages');
+$first_page = $xml_doc->createElement('first_page',$first_page);
+$last_page = $xml_doc->createElement('last_page',$last_page);
+
+
+$count = 1;
+$journalx = $xml_doc->getElementsByTagName('journal_article');
+foreach($journalx as $item){
+       
+        $count++; 
+
+
+}
+
+
+	
+$num = sprintf("%02d", $count);
+$cc = $num;
+
+
+
+
+
+
+$dd = "".$doi.".".$cc;
+$dd = preg_replace('/\s+/', '', $dd);
+
+
+
+
+$doi_link = "10.24849/j.geot.".$dd;
+$doi_data = $xml_doc->createElement('doi_data');
+$doi = $xml_doc->createElement('doi',$doi_link);
+$resource = $xml_doc->createElement('resource',$ficheiro_link);
+
+
+######## COMEÇA AQUI
+
+$journal->appendChild($journal_article);
+$journal_article->appendChild($titles);
+$journal_article->appendChild($journal_atr);
+$titles->appendChild($title);
+$titles->appendChild($original_language_title);
+$journal_article->appendChild($contributors);
+
+// Aqui tenho de geral um loop para possibilitar vários contributors e usar explode para separar paramentros
+$x = 0;
+foreach($textAr as $line) {
+ 
+    // Loop para diferenciar nomes para add childs
+    $arr = array('a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','x','y','z');
+
+    //echo $contributors[0];
+    //echo $contributors[1];
+    //echo $contributors[2];
+    $i = $x;
+
+    $var = $arr[$i];
+    
+    $contributors->appendChild(${"person_name_$i"});
+    ${"person_name_$i"}->appendChild(${"given_name_$i"});
+	${"person_name_$i"}->appendChild(${"sur_name_$i"});
+	$contributors->appendChild(${"orcid_$i"});
+	$contributors->appendChild(${"organization_$i"});
+    $x++; 
+
+}
+
+///
+
+$journal_article->appendChild($publication_date);
+$publication_date->appendChild($pub_Attribute);
+$publication_date->appendChild($month);
+$publication_date->appendChild($year);
+$journal_article->appendChild($publication_date_o);
+$publication_date_o->appendChild($pub_Attribute_b);
+$publication_date_o->appendChild($month_b);
+$publication_date_o->appendChild($year_b);
+
+$journal_article->appendChild($pages);
+$pages->appendChild($first_page);
+$pages->appendChild($last_page);
+
+$journal_article->appendChild($doi_data);
+$doi_data->appendChild($doi);
+$doi_data->appendChild($resource);
+
+$formatx = ".xml";
+$s = $replace.$formatx;
+$done = $xml_doc->save($s);
+
+
+
+// Atualizar db pois ja adicionamos este artigo ao XML
+$docid = $fetch['id'];
+$update = $connect->query("UPDATE issueslist SET xml_add = 1 WHERE id = '$docid'");
+
+}
---
+++ create_xml.php
@@ -0,0 +1,127 @@
+<?php
+define('BASE_URL', 'http://acessospgeotecnia.w30.mycloud.pt');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+$config['database']['host'] = "localhost";
+$config['database']['user'] = "spg";
+$config['database']['pass'] = "spg02";
+$config['database']['db'] = "spg";
+
+/*==========================  CUSTOM-PHP EDIT XML  ============================== */
+/*==========================     Version 3.0     ============================= */
+
+/*
+        Developed by Gonçalo M
+ */
+
+$connect = new mysqli($config['database']['host'],$config['database']['user'],$config['database']['pass'],$config['database']['db']);  
+
+
+// Verificar inputs que tenham 0 em XML_ADD
+
+$check = $connect->query("SELECT * FROM issues WHERE xml_add = 0");
+while($fetch = $check->fetch_array(MYSQLI_ASSOC)){
+
+$xml_file = $fetch['xml_file'];
+
+$idx = $fetch['id'];
+
+
+$url = "http://spgeotecnia.w30.mycloud.pt/uploads/";
+$file_url = $xml_file; // vem da db
+$original = $url;
+$replace = str_replace(array($url,".pdf"),array("",""),$file_url);
+//$doi = $replace;
+$ano = date("Y");
+// Adicionar ponto pois é removide quando adicionado aos uploads
+$ano_a = date("Y").".";
+$rx = str_replace($ano,$ano_a,$replace);
+$doi = $rx;
+
+
+$replace_b = substr_replace($doi, '', 8);
+$volume = $replace_b;
+
+$replace_c = substr_replace($doi, '', 0,-2);
+$doi_sub = $replace_c;
+
+$mes = date('m');
+$ano = date("Y");
+$timestamp = date('mdYHis');
+
+$myfile = fopen( $doi.".xml", "w") or die("Unable to open file!");
+$xmlstr = <<<XML
+<!--?xml version="1.0" encoding="UTF-8"?-->
+<getautomb_key>
+<doi_batch version="4.3.7" xmlns="http://www.crossref.org/schema/4.3.7" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.crossref.org/schema/4.3.7 http://www.crossref.org/schema/deposit/crossref4.3.7.xsd">
+<head>
+<doi_batch_id>spggeot142</doi_batch_id>
+<timestamp>$timestamp</timestamp>
+<depositor>
+  <depositor_name>portgeo</depositor_name> 
+  <email_address>arquivo@revistageotecnia.com</email_address>
+</depositor>
+<registrant>portgeo</registrant> 
+</head>
+<body>
+<journal>
+  <journal_metadata>
+    <full_title>Geotecnia</full_title>
+    <abbrev_title>Geot</abbrev_title>
+    <issn media_type='print'>03799522</issn>
+    <issn media_type='electronic'>03799522</issn>
+    <doi_data><doi>10.24849/j.geot</doi> <resource>http://spgeotecnia.w30.mycloud.pt/repository/$idx</resource> 
+    </doi_data>
+  </journal_metadata>
+  <journal_issue>
+    <publication_date media_type='print'>
+      <month>$mes</month>
+      <year>$ano</year>
+    </publication_date>
+    <publication_date media_type='online'>
+      <month>$mes</month>
+      <year>$ano</year>
+    </publication_date>
+    <journal_volume>
+      <volume>$doi_sub</volume>
+    </journal_volume>
+    <doi_data>
+      <doi>10.24849/j.geot.$volume</doi><resource>$file_url</resource> 
+    </doi_data>
+  </journal_issue>
+  <!-- ============== -->
+  
+  <!-- ============== -->
+</journal>
+</body>
+</doi_batch>
+</getautomb_key>
+XML;
+
+$txt = $xmlstr;
+
+fwrite($myfile, $txt);
+
+fclose($myfile);
+
+
+// Atualizar db pois ja adicionamos este artigo ao XML
+$docid = $fetch['id'];
+$update = $connect->query("UPDATE issues SET xml_add = 1 WHERE id = '$docid'");
+
+
+$param = $doi.".xml";
+$order = 0;
+
+
+if($connect->query("INSERT INTO uploads(file) VALUES('$param')")){
+
+  //echo "Completo";
+
+}else{
+  $error = $connect->error;
+  echo $error;
+}
+
+
+}
